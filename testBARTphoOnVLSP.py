29ceea8fca41c7f4058bd34deb009e9a7b0d0ca3
Le Manh Ha <lemh329@gmail.com>
2022-11-03 10:45:31 +0700

    update

--- testBARTphoOnVLSP.py
+++ testBARTphoOnVLSP.py
@@ -1,20 +1,17 @@
-# python temp/testBARTphoOnVLSP.py -m finetune-bartpho-newscorpus50 -i VLSP_Data/vlsp_abmusu_test_data.jsonl -o VLSP_Data/results.txt
-# python temp/testBARTphoOnVLSP.py -m finetune-bartpho-newscorpus50/checkpoint-48158 -i VLSP_Data/vlsp_2022_abmusu_validation_data_new.jsonl -o VLSP_Data/vlsp_validation_results.txt --evaluationPath VLSP_Data/vlsp_validation_evaluation.json
+# python temp/testBARTphoOnVLSP.py -m finetune-bartpho-newscorpus50/checkpoint-96316 -i VLSP_Data/LexRank/vlsp2022_test_full.json -o VLSP_Data/LexRank/results.txt
 
 import json, getopt, sys
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from rouge import Rouge
 
 # Setup arguments
 argList = sys.argv[1:]
-options = "m:i:o:e"
-longOptions = ["modelPath=", "inputPath=", "outputPath=", "evaluationPath="]
+options = "m:i:o"
+longOptions = ["modelPath=", "inputPath=", "outputPath="]
 
 # Default arguments
 modelPath = "finetune-bartpho-newscorpus50"
 inputPath = "VLSP_Data/vlsp_abmusu_test_data.jsonl"
 outputPath = "VLSP_Data/results.txt"
-evaluationPath = ""
 
 # Parse arguments
 try:
@@ -30,9 +27,6 @@ try:
         elif arg in ("-o", "--outputPath"):
             print("Output: %s" % val)
             outputPath = val
-        elif arg in ("-e", "--evaluationPath"):
-            print("Evaluation: %s" % val)
-            evaluationPath = val
 except getopt.error as err:
     print(str(err))
 
@@ -40,26 +34,15 @@ except getopt.error as err:
 tokenizer = AutoTokenizer.from_pretrained(modelPath)
 model = AutoModelForSeq2SeqLM.from_pretrained(modelPath)
 
-# init rouge
-rougeScorer = Rouge()
-
-def getSingleDocument(documents):
-    return '.'.join(document["raw_text"] for document in documents)
-
-def getSummary(multidocs, evaluate):
+def getSummary(multidocs):
     jsonObject = json.loads(multidocs)
-    documents = jsonObject["single_documents"]
-    text = getSingleDocument(documents)
+    text = jsonObject["raw_text"]
 
     tokens_input = tokenizer.encode(text, return_tensors='pt', max_length=512, truncation=True)
     summary_ids = model.generate(tokens_input, min_length=256, max_length=512)
     prediction = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-    scores = rougeScorer.get_scores(prediction, jsonObject["summary"]) if evaluate else None;
-
-    summary = {"prediction": prediction, "scores": scores}
-    print(summary)
-    return summary
+    return prediction
 
 def process():
     # read input
@@ -69,19 +52,13 @@ def process():
     # calculate summaries
     summaries = []
     for line in lines:
-        summary = getSummary(line, evaluationPath != "")
+        summary = getSummary(line)
         summaries.append(summary)
-        print("Processed %d / %d" % (len(summaries), len(lines))
+        print("Processed %d / %d" % (len(summaries), len(lines)))
 
     # write prediction to output
     with open(outputPath, "w", encoding='utf-8') as output:
         output.write("\n".join([summary["prediction"] for summary in summaries]))
         output.close()
-
-    # write prediction with rouge scores
-    if evaluationPath != "":
-        with open(evaluationPath, "w", encoding='utf-8') as evaluation:
-            evaluation.write("\n".join([json.dumps(summary, ensure_ascii=False, indent = 4) for summary in summaries]))
-            evaluation.close()
 
 process()
